446c87414a10fa0320a5020dd206cc7b83b54bb0
annbrodsky <anna.brodsky@wsj.com>
2025-09-03 16:11:30 -0400

    Create piano-for-tealium.js

---
+++ piano-for-tealium.js
@@ -0,0 +1,19 @@
+const host = window.location.hostname;
+  // const isQa = host.includes('dev.'); 
+const isProd = utag_data['ut.env'] === 'prod';
+const parts = host.split('.');
+const root = parts[parts.length - 2];
+let domain = 'https://www.wsj.com';
+if (WHITELIST.includes(root)) {      
+     // domain = `https://www.${isQa ? 'dev.' : ''}${root}.com`;
+    domain = 'https://www.' + (isProd ? '' : 'dev.') + root + '.com';
+}
+
+ (function() {
+    const scriptEl = document.createElement('script');
+    scriptEl.type = 'text/javascript';
+    scriptEl.defer = true;
+    scriptEl.src = domain + '/asset/piano/piano.js';
+    const targetEl = document.getElementsByTagName('script')[0];
+    targetEl.parentNode.insertBefore(scriptEl, targetEl);
+})();    
